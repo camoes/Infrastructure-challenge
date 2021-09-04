0c8d43fe087cdd43ec46fa71b5f3312c89ebaaa2
admin <admin@admins-MacBook-Pro.local>
2021-09-04 13:16:48 +0200

    Adding modules to the tf repo

--- main.tf
+++ main.tf
@@ -4,25 +4,18 @@ terraform {
   backend "s3" {
     bucket         = "kyler-github-actions-demo-terraform-tfstate"
     key            = "terraform.tfstate"
-    region         = "us-east-1"
+    region         = "${var.region}"
     dynamodb_table = "aws-locks"
     encrypt        = true
   }
 }
 
-# Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
-provider "aws" {
-  region  = "us-east-1"
-}
-
-//Uploading the needed scripts from within the repo, this should be settled as a different repo in the future, but that is beyond the scope of this challenge
-
 module "s3" {
-  source = "../modules/s3"
+  source = "./modules/s3"
 }
 
 module "lambdas" {
-  source   = "../modules/lambdas"
+  source   = "./modules/lambdas"
 }
 
 
