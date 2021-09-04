1e8f2d0dcbdecd03d45c150517121cacdd235929
admin <admin@admins-MacBook-Pro.local>
2021-09-04 19:00:51 +0200

    First version of redirection Lambda

--- files/producer.py
+++ files/producer.py
@@ -1,8 +1,15 @@
 import boto3
 
 def lambda_handler (event, context):
+    
     lambda_client = boto3.client('lambda')
     lambda_payload = event.get('data')
     lambda_client.invoke(FunctionName='consumer', 
                         InvocationType='Event',
                         Payload=lambda_payload)
+    response = {
+            'statusCode': 200,
+            'header': {'Content-Type': 'application/json'},
+            'body': 'path to file'
+    } 
+    return response
