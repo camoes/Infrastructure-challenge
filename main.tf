a9af5f2b03c7201f5dd90b27b515b31ccc675c90
admin <admin@admins-MacBook-Pro.local>
2021-09-06 17:12:10 +0200

    First tf push

--- main.tf
+++ main.tf
@@ -4,8 +4,6 @@ terraform {
   backend "s3" {
     bucket         = "kyler-github-actions-demo-terraform-tfstate"
     key            = "terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "aws-locks"
     encrypt        = true
   }
 }
