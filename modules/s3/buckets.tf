a0389192985650b50890f97e50c2cc272bd8b701
admin <admin@admins-MacBook-Pro.local>
2021-09-07 10:13:23 +0200

    Filling missing paths for source data

--- modules/s3/buckets.tf
+++ modules/s3/buckets.tf
@@ -1,15 +1,9 @@
 provider "aws" {
   region = var.aws_region
 }
-resource "random_pet" "lambda_bucket_name" {
-  
-    # Generate a new pet name each time we switch to a new bucket id
-  prefix = "vimcar-challenge"
-  length = 4
-  
-}
+
 resource "aws_s3_bucket" "lambda_bucket_producer" {
-  bucket = random_pet.lambda_bucket_name.id
+  bucket = consumer.lambda_bucket_name.id
 
   acl           = "private"
   force_destroy = true
@@ -30,7 +24,7 @@ resource "aws_s3_bucket_object" "lambda_producer_object" {
   etag = filemd5(data.archive_file.lambda_producer_file.output_path)
 }
 resource "aws_s3_bucket" "lambda_bucket_consumer" {
-  bucket = random_pet.lambda_bucket_name.id
+  bucket = producer.lambda_bucket_name.id
 
   acl           = "private"
   force_destroy = true
