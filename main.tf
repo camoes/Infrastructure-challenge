2c0a5f44eff471ec6b7d202a04f8caa680be8014
admin <admin@admins-MacBook-Pro.local>
2021-09-07 09:41:06 +0200

    Adding correct S3 refereences for the lambdas

--- main.tf
+++ main.tf
@@ -14,13 +14,13 @@ terraform {
 module "s3" {
   source = "./modules/s3"
 }
-module "lambdas" {
-  source = "./modules/lambdas"
-}
+// module "lambdas" {
+//  source = "./modules/lambdas"
+//}
 
-module "apigateway" {
-  source = "./modules/apigateway"
-}
+//module "apigateway" {
+//  source = "./modules/apigateway"
+//}
 
 
 //module "cloudwatch" {
