70097f28546699273303a4378756f866ac40bc26
admin <admin@admins-MacBook-Pro.local>
2021-09-07 08:45:53 +0200

    Loading needed refereences between modules

--- modules/apigateway/apigateway.tf
+++ modules/apigateway/apigateway.tf
@@ -34,7 +34,9 @@ resource "aws_cloudwatch_log_group" "api_gw" {
 
   retention_in_days = 30
 }
-
+data "aws_lambda" "producer" {
+  id = var.lambda_id
+}
 // we check the request is in the desired json format 
 resource "aws_apigatewayv2_integration" "producer" {
   source = "../lambdas/lambdas.tf"
