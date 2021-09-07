9df6b7479534acfa94d8328bab81ead3e135c5ce
admin <admin@admins-MacBook-Pro.local>
2021-09-07 09:03:44 +0200

    Loading needed refereences between modules

--- modules/lambdas/output.tf
+++ modules/lambdas/output.tf
@@ -1,11 +1,11 @@
 
-output "function_name" {
+output "function_name_producer" {
   description = "Name of the Lambda function producer."
 
   value = aws_lambda_function.producer.function_name
 }
 
-output "function_name" {
+output "function_name_consumer" {
   description = "Name of the Lambda function consumer."
 
   value = aws_lambda_function.consumer.function_name
