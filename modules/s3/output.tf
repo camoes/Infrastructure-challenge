ea4b1861713183377dca0b4b46d7acad85c0912b
admin <admin@admins-MacBook-Pro.local>
2021-09-07 09:26:24 +0200

    Loading needed refereences between modules

--- modules/s3/output.tf
+++ modules/s3/output.tf
@@ -1,7 +1,7 @@
 output "lambda_producer" {
-  value = aws_s3_bucket.lambada_bucket_producer.id
+  value = aws_s3_bucket.lambda_bucket_producer.id
 }
 
 output "lambda_consumer" {
-  value = aws_s3_bucket.lambada_bucket_consumer.id
+  value = aws_s3_bucket.lambda_bucket_consumer.id
 }
