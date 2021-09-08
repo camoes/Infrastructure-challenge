bff13b7f9ac3c220010656a8099139275c7822ee
admin <admin@admins-MacBook-Pro.local>
2021-09-08 17:19:22 +0200

    Fxing python bad reference

--- modules/s3_vimcar/buckets.tf
+++ modules/s3_vimcar/buckets.tf
@@ -30,7 +30,7 @@ resource "aws_s3_bucket" "lambda_bucket_consumer" {
 }
 data "archive_file" "lambda_consumer_file" {
   type = "zip"
-  source_file = "${path.root}/files/producer/producer.py"
+  source_file = "${path.root}/files/consumer/consumer.py"
   output_path = "${path.root}/files/consumer.zip"
 }
 
