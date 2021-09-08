8d4668912ff0f64df4b1ef1eea53a9655df0f7aa
admin <admin@admins-MacBook-Pro.local>
2021-09-08 18:49:14 +0200

    Creating custom permissions

--- modules/lambdas_vimcar/lambdas.tf
+++ modules/lambdas_vimcar/lambdas.tf
@@ -62,5 +62,5 @@ resource "aws_iam_role" "lambda_exec" {
 
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRoleForLambda"
+  policy_arn = "arn:aws:iam::251673427141:policy/serveless_lambda_vimcar"
 }
