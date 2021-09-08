1b175ca8e1a94d6d493c8030101735edfce9f8f2
admin <admin@admins-MacBook-Pro.local>
2021-09-08 17:15:43 +0200

    Fxing python bad reference

--- files/producer/producer.py
+++ files/producer/producer.py
@@ -1,17 +1,21 @@
-import boto3
 import json
-
-lambda_client = boto3.client('lambda')
-
-def lambda_handler (event, context):
-    #his section launches the lambda functtion consumer and send the event as data payload
+import boto3
+ 
+# Define the client to interact with AWS Lambda
+client = boto3.client('lambda')
+ 
+def lambda_handler(event,context):
+ 
+    # Define the input parameters that will be passed
+    # on to the child function
     lambda_payload = event.get('data')
-    lambda_client.invoke(FunctionName='consumer_lambda', 
-                        InvocationType='Event',
-                        Payload=lambda_payload)
-    response = {
-            'statusCode': 200,
-            'header': {'Content-Type': 'application/json'},
-            'body': 'path to file'
-    }  #returns 200 from the previous message , this part needs refines 
-    return response
+    response = client.invoke(
+        FunctionName = 'arn:aws:lambda:eu-west-1:890277245818:function:consumer_lambda',
+        InvocationType = 'RequestResponse',
+        Payload = json.dumps(lambda_payload)
+    )
+ 
+    responseFromChild = json.load(response['Payload'])
+ 
+    print('\n')
+    print(responseFromChild)
