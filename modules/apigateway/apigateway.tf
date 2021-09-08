8934eeaf7879acb2b424e3122ab08ba6a79d3234
admin <admin@admins-MacBook-Pro.local>
2021-09-08 14:11:03 +0200

    Bad function name refereence

--- modules/apigateway/apigateway.tf
+++ modules/apigateway/apigateway.tf
@@ -62,7 +62,7 @@ resource "aws_apigatewayv2_route" "producer_route" {
   api_id = aws_apigatewayv2_api.lambda.id
 
   route_key = "$default" //This should be changed for the desired endpoint to trigger de request for the lambda, which hasn't been defined 
-  target    = "integrations/${aws_apigatewayv2_integration.producer.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
 }
 
 
