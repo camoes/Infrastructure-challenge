3e58df4cd4209a24ae211c7f38a34146d3eb163f
admin <admin@admins-MacBook-Pro.local>
2021-09-08 18:30:31 +0200

    Understanding lambda hanblder nomenclature

--- files/producer/producer.py
+++ files/producer/producer.py
@@ -3,13 +3,13 @@ import boto3, json
 import json
 
 def lambda_handler(event, context):
-    ambda_client = boto3.client('lambda')
 
-test_event = dict()
+        lambda_client = boto3.client('lambda')
+        test_event = dict()
 
-response = lambda_client.invoke(
-        FunctionName='arn:aws:lambda:eu-west-1:251673427141:function:consumer_lambda',
-        Payload=json.dumps(test_event),
-        )
-print(response['Payload'])
-print(response['Payload'].read().decode("utf-8"))
+        response = lambda_client.invoke(
+                FunctionName='arn:aws:lambda:eu-west-1:251673427141:function:consumer_lambda',
+                Payload=json.dumps(test_event),
+                )
+        print(response['Payload'])
+        print(response['Payload'].read().decode("utf-8"))
