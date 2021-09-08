1c5cb3d944c44588e9cb656c8705c355fd182177
admin <admin@admins-MacBook-Pro.local>
2021-09-08 12:54:35 +0200

    S3_KEY needed

--- modules/lambdas_vimcar/lambdas.tf
+++ modules/lambdas_vimcar/lambdas.tf
@@ -37,8 +37,3 @@ resource "aws_cloudwatch_log_group" "consumer" {
 
   retention_in_days = 30
 }
-
-resource "aws_iam_role_policy_attachment" "lambda_policy" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
