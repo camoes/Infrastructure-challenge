1b739d93160919f14159da2007d942ac6965e046
admin <admin@admins-MacBook-Pro.local>
2021-09-08 21:37:14 +0200

    Adding Dashboards

--- modules/cloudwatch-metrics/variables.tf
+++ modules/cloudwatch-metrics/variables.tf
@@ -1,41 +1,9 @@
-variable alarm_type {
-  type        = list(string) 
-  description = "Different Alarm Types"
-}
 
-variable evaluation_periods{
-  type        = list(string) 
-  description = "Evaluation Periods"
-}
 
-variable metric_name {
-  type        = list(string) 
-  description = "Metric Name"
-}   
+# Define instance id
+variable "aws_region" {
+  description = "AWS region for all resources."
 
-variable name_space {
-  type        = list(string) 
-  description = "Name Space"
-}
-
-variable period {
-  type        = list(string) 
-  description = "Period"
-}
-
-variable statistic {
-  type        = list(string) 
-  description = "Statistic"
-}
-
-variable threshold {
-  type        = list(string) 
-  description = "Threshold"
-}
-
-# Define instance ids
-
-variable instance_id {
-  type        = list(string) 
-  description = "ID of each instance"
+  type    = string
+  default = "eu-west-1"
 }
