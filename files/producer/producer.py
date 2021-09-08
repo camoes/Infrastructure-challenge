55c5c89aae9176ad43b7219eb2efa1951a7239a9
admin <admin@admins-MacBook-Pro.local>
2021-09-08 17:48:31 +0200

    Fxing python bad reference

--- files/producer/producer.py
+++ files/producer/producer.py
@@ -1,13 +1,15 @@
 import boto3, json
 
-lambda_client = boto3.client('lambda')
 
-test_event = dict()
+def lambda_handler(event, context):
+        lambda_client = boto3.client('lambda')
 
-response = lambda_client.invoke(
-  FunctionName='arn:aws:lambda:eu-west-1:251673427141:function:consumer_lambda',
-  Payload=json.dumps(test_event),
-)
+        test_event = dict()
 
-print(response['Payload'])
-print(response['Payload'].read().decode("utf-8"))
+        response = lambda_client.invoke(
+        FunctionName='arn:aws:lambda:eu-west-1:251673427141:function:consumer_lambda',
+        Payload=json.dumps(test_event),
+        )
+
+        print(response['Payload'])
+        print(response['Payload'].read().decode("utf-8"))
