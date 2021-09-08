7abc8f458759e0ee06fd8d909b1c2e613cc3285c
admin <admin@admins-MacBook-Pro.local>
2021-09-08 12:51:31 +0200

    S3_KEY needed

--- modules/lambdas_vimcar/lambdas.tf
+++ modules/lambdas_vimcar/lambdas.tf
@@ -37,22 +37,6 @@ resource "aws_cloudwatch_log_group" "consumer" {
 
   retention_in_days = 30
 }
-resource "aws_iam_role" "lambda_exec" {
-  name = "serverless_lambda"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Sid    = ""
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-      }
-    ]
-  })
-}
 
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_exec.name
