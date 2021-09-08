d704e0235a5181cc63e410c7f15bd995fcb178ac
admin <admin@admins-MacBook-Pro.local>
2021-09-08 20:33:17 +0200

    Creating custom permissions

--- files/producer/producer.py
+++ files/producer/producer.py
@@ -4,7 +4,7 @@ import boto3, json
 def lambda_handler(event, context):
 
         lambda_client = boto3.client('lambda')
-        data = event['data']
+        data = event['body']
 
         response = lambda_client.invoke(
                 FunctionName='arn:aws:lambda:eu-west-1:251673427141:function:consumer_lambda',
