bc64210ebd88ed4ff0e9ac7760c794e1e441f793
admin <admin@admins-MacBook-Pro.local>
2021-09-08 19:25:01 +0200

    Creating custom permissions

--- files/consumer/consumer.py
+++ files/consumer/consumer.py
@@ -8,7 +8,7 @@ logger.setLevel(logging.INFO)
 def lambda_handler(event, context):
     data = event.get('data')
 #FizzBuzz section of the stored array 
-    for i in range(data):
+    for i in data:
         if data[i]%3==0 and data[i]%5==0:
             data[i] = "fizzbuzz"
         elif data[i]%3==0:
