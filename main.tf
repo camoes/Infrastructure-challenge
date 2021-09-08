075bbb54aa20e6ae77b194e81694e20fc1948f89
admin <admin@admins-MacBook-Pro.local>
2021-09-08 10:49:01 +0200

    required providers

--- main.tf
+++ main.tf
@@ -1,6 +1,21 @@
 # Require TF version to be same as or greater than 0.12.13
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.48.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+  }
   required_version = ">=0.12.13"
+  
   backend "s3" {
     bucket         = "vimcar-challenge-tfestate-cmontesinos"
     key            = "terraform.tfstate"
@@ -16,9 +31,9 @@ module "s3_vimcar" {
 module "lambdas_vimcar" {
   source                    = "./modules/lambdas_vimcar"
   lambda_bucket_producer    = module.s3_vimcar.lambda_producer
-  lambda_bucket_consumer    = "${module.s3_vimcar.lambda_consumer}"
-  source_code_hash_consumer = "${module.s3_vimcar.lambda_consumer_file}"
-  source_code_hash_producer = "${module.s3_vimcar.lambda_producer_file}"
+  lambda_bucket_consumer    = module.s3_vimcar.lambda_consumer
+  source_code_hash_consumer = module.s3_vimcar.lambda_consumer_file
+  source_code_hash_producer = module.s3_vimcar.lambda_producer_file
 
 }
 
